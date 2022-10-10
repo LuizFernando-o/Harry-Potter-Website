93df4b804610d6fe4413530443b21663b44b48d4
luizfernando-o <luiz.2019230186@unicap.br>
2022-10-10 20:15:44 -0300

    house characters.js

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -38,7 +38,7 @@ const displayCharacters = (characters) => {
             return `
             <li class="character">
                 <h2>${character.name}</h2>
-                <p>Escola de Magia: ${character.house!=""? character.house : "no-data"}</p>
+                <p>Casa: ${character.house!=""? character.house : "no-data"}</p>
                 <p>Data de Nascimento: ${character.dateOfBirth!=""? character.dateOfBirth : "no-data"}</p>              
                 <img src="${character.image!=""? character.image : '../assets/img/no-img.png'}"></img>
             </li>
